c6262894b7e05c036cf7ca88c87cd1ecc4bb57fc
mitheelgajare <mitheelg08@gmail.com>
2021-10-15 19:27:35 +0530

    Completed project (hopefully)

--- client/pages/about.tsx
+++ client/pages/about.tsx
@@ -2,7 +2,25 @@ import React, { useState, useEffect } from "react";
 import { NextPage } from "next";
 
 const About: NextPage = () => {
-  return <div>about page</div>;
+  return (
+    <div className="about-page">
+      <h1 className="title">About Us</h1>
+      <div className="wrapper">
+        <h2>Commercial Math Calculator ( CMC )</h2>
+        <p>
+          You will be able to perform every calculation, be it simple or complex
+          on this web app. <br />
+          Every calculation means all commercial calculations such as profit,
+          loss, p%, l%, discount, mp etc.
+        </p>{" "}
+        <br />
+        <p>
+          This is an open source app written in NextJS and Sass. <br />
+          License: MIT
+        </p>
+      </div>
+    </div>
+  );
 };
 
 export default About;
