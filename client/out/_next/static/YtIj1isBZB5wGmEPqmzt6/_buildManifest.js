be76ea836ee745c08b59b6925a47190541f4bc63
mitheelgajare <mitheelg08@gmail.com>
2021-11-07 19:43:13 +0530

    feat: Included out folder for Continuous deployment

---
+++ client/out/_next/static/YtIj1isBZB5wGmEPqmzt6/_buildManifest.js
@@ -0,0 +1,1 @@
+self.__BUILD_MANIFEST={__rewrites:{beforeFiles:[],afterFiles:[],fallback:[]},"/":["static/chunks/pages/index-d481f2f9f58305f8.js"],"/_error":["static/chunks/pages/_error-2280fa386d040b66.js"],"/about":["static/chunks/pages/about-76e9a673e29960dd.js"],sortedPages:["/","/_app","/_error","/about"]},self.__BUILD_MANIFEST_CB&&self.__BUILD_MANIFEST_CB();
